7812707336589dddb5e5e085ba16360f9855df8c
Raymond Guo <guorlei@google.com>
2020-06-09 20:45:22 +0000

    Add license to files and basic anti-patterns

--- test/test-app/src/app/app.component.ts
+++ test/test-app/src/app/app.component.ts
@@ -3,8 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'test-app';
+  greetingA;
+  greetingB;
+
+  constructor() {
+    this.greetingA = this.maybeHello('Alice');
+    this.greetingB = this.maybeHello('Bob');
+  }
+
+  // Basic test: --noImplicitReturns
+  maybeHello(name: string) {
+    if (name === 'Bob') {
+      return 'Hello';
+    }
+    return undefined;
+  }
+
+  // Basic test: --noImplicitAny
+  sayHello(name) {
+    return name;
+  }
 }
