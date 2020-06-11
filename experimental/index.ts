6931008f3b93f10cdd2024334645d6cda967211c
Raymond Guo <guorlei@google.com>
2020-06-11 02:05:47 +0000

    Initialize npm package

---
+++ experimental/index.ts
@@ -0,0 +1,15 @@
+import { Project } from "ts-morph";
+
+const project = new Project({
+  tsConfigFilePath: "./test/test-app/tsconfig.json",
+});
+
+// ...lots of code here that manipulates, copies, moves, and deletes files...
+console.log("test");
+
+const sourceFiles = project.getSourceFiles();
+
+console.log(sourceFiles);
+
+// when you're all done, call this and it will save everything to the file system
+// await project.save();
